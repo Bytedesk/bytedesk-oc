53136eb753b9f501d0d7aeb1e0f0b5a2115e870b
jack ning <github@bytedesk.com>
2023-09-02 08:57:48 +0800

    update

--- bytedesk-oc.podspec
+++ bytedesk-oc.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |spec|
   spec.name         = "bytedesk-oc"
-  spec.version      = "0.1.4"
+  spec.version      = "0.1.5"
   spec.summary      = "bytedesk.com helpdesk chat kefu lib."
   spec.description  = <<-DESC
   Online chat kefu Lib for ios, Helpdesk system
--- bytedesk-oc/bytedesk_oc.h
+++ bytedesk-oc/bytedesk_oc.h
@@ -16,32 +16,32 @@ FOUNDATION_EXPORT const unsigned char bytedesk_ocVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <bytedesk_oc/PublicHeader.h>
 
 //
-#import <bytedesk_oc/ui/apis/BDUIApis.h>
+#import <bytedesk_oc/BDUIApis.h>
 //
-#import <bytedesk_oc/core/apis/BDCoreApis.h>
-#import <bytedesk_oc/core/apis/BDConfig.h>
-#import <bytedesk_oc/core/apis/BDMQTTApis.h>
-#import <bytedesk_oc/core/apis/BDDBApis.h>
-#import <bytedesk_oc/core/apis/BDHttpApis.h>
+#import <bytedesk_oc/BDCoreApis.h>
+#import <bytedesk_oc/BDConfig.h>
+#import <bytedesk_oc/BDMQTTApis.h>
+#import <bytedesk_oc/BDDBApis.h>
+#import <bytedesk_oc/BDHttpApis.h>
 //
-#import <bytedesk_oc/core/util/BDConstants.h>
-#import <bytedesk_oc/core/util/BDUtils.h>
-#import <bytedesk_oc/core/util/BDSettings.h>
-#import <bytedesk_oc/core/util/BDNotify.h>
+#import <bytedesk_oc/BDConstants.h>
+#import <bytedesk_oc/BDUtils.h>
+#import <bytedesk_oc/BDSettings.h>
+#import <bytedesk_oc/BDNotify.h>
 //
-#import <bytedesk_oc/core/model/BDThreadModel.h>
-#import <bytedesk_oc/core/model/BDQueueModel.h>
-#import <bytedesk_oc/core/model/BDMessageModel.h>
-#import <bytedesk_oc/core/model/BDContactModel.h>
-#import <bytedesk_oc/core/model/BDGroupModel.h>
-#import <bytedesk_oc/core/model/BDWorkGroupModel.h>
-#import <bytedesk_oc/core/model/BDNoticeModel.h>
-#import <bytedesk_oc/core/model/BDCategoryModel.h>
-#import <bytedesk_oc/core/model/BDArticleModel.h>
-#import <bytedesk_oc/core/model/BDFeedbackModel.h>
-#import <bytedesk_oc/core/model/BDTicketModel.h>
-#import <bytedesk_oc/core/model/BDTodoModel.h>
-#import <bytedesk_oc/core/model/BDBlockModel.h>
-#import <bytedesk_oc/core/model/BDLeaveMsgModel.h>
-#import <bytedesk_oc/core/model/BDProfileModel.h>
-#import <bytedesk_oc/core/model/ExtraParam.h>
+#import <bytedesk_oc/BDThreadModel.h>
+#import <bytedesk_oc/BDQueueModel.h>
+#import <bytedesk_oc/BDMessageModel.h>
+#import <bytedesk_oc/BDContactModel.h>
+#import <bytedesk_oc/BDGroupModel.h>
+#import <bytedesk_oc/BDWorkGroupModel.h>
+#import <bytedesk_oc/BDNoticeModel.h>
+#import <bytedesk_oc/BDCategoryModel.h>
+#import <bytedesk_oc/BDArticleModel.h>
+#import <bytedesk_oc/BDFeedbackModel.h>
+#import <bytedesk_oc/BDTicketModel.h>
+#import <bytedesk_oc/BDTodoModel.h>
+#import <bytedesk_oc/BDBlockModel.h>
+#import <bytedesk_oc/BDLeaveMsgModel.h>
+#import <bytedesk_oc/BDProfileModel.h>
+#import <bytedesk_oc/ExtraParam.h>
